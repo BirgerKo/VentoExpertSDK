3bf4243c13785784f8cacdc210ea98963f9c20a7
Birger <35929926+BirgerKo@users.noreply.github.com>
2025-01-30 19:44:00 +0100

    updated supported ventilator fans list

--- VentoExpertSDK/__init__.py
+++ VentoExpertSDK/__init__.py
@@ -1,8 +1,15 @@
 """
 Blauberg Vento Expert SDK
 
-This is a Python module for making a connection to a fans tha support Blaubergs API
-https://blaubergventilatoren.de/uploads/download/b133_4_1en_01preview.pdf
+This is a Python module for making connections to ventilator fans that support Blaubergs API
+https://blaubergventilatoren.de/uploads/download/b133_4_1en_02_preview.pdf
+
+VENTO Expert A30 W V.2
+VENTO Expert A50-1 W V.2
+VENTO Expert A85-1 W V.2
+VENTO Expert A100-1 W V.2
+VENTO Expert Duo A30-1 W V.2
+VENTO Expert A50-1 W V.3
 
 see http://www.dingus.dk for more information
 """
